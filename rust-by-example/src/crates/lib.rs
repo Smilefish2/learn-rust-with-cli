1bf13d1ba04bd6b06b232c914b46be51cc78aea3
JMJ <yang.jmj@gmail.com>
2021-12-09 15:56:44 +0800

    Crates Creating a Library 2

---
+++ rust-by-example/src/crates/lib.rs
@@ -0,0 +1,38 @@
+use std::{env};
+use std::process::Command;
+use clap::{App, ArgMatches};
+
+pub const NAME: &'static str = "rust-by-example:crates/lib";
+const ABOUT: &'static str = "https://rustwiki.org/zh-CN/rust-by-example/crates/lib.html";
+
+pub fn sub_command<'help>() -> App<'help> {
+    let sub_command = App::new(NAME)
+        .about(ABOUT);
+    return sub_command;
+}
+
+/**
+编译 rary.rs
+*/
+pub fn sub_handler(_matches :&ArgMatches){
+
+    // println!("手动执行命令：cd rust-by-exampl/src/crates && rustc --crate-type=lib rary.rs && ls && cd ../../../";
+
+    let current_dir = env::current_dir().unwrap();
+    println!(
+        "Entries modified in the last 24 hours in {:?}:",
+        current_dir
+    );
+
+    let pwd = current_dir.into_os_string().into_string().unwrap();
+    let command = format!("cd {}/rust-by-example/src/crates && rustc --crate-type=lib rary.rs && ls", pwd);
+
+    println!(
+        "command = {}",
+        command
+    );
+
+    let output = Command::new(command)
+        .output().expect("执行异常，提示");
+    println!("{:?}", output);
+}
