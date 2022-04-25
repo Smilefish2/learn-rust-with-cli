db1050d5bcfdee5076a3b2437778c32cf1978dee
JMJ <yang.jmj@gmail.com>
2022-04-25 18:26:19 +0800

    update

--- rust-course/src/compound_type/string_slice.rs
+++ rust-course/src/compound_type/string_slice.rs
@@ -14,7 +14,31 @@ fn greet(name: &str) {
     println!("Hello, {}!", name);
 }
 
+fn first_word(s: &String) -> &str {
+    &s[..1]
+}
+
 pub fn sub_handler(_matches :&ArgMatches){
     let my_name = "Pascal";
     greet(my_name);
+
+    let s = String::from("hello world");
+
+    let hello = &s[0..5];
+    let world = &s[6..11];
+
+    println!("hello, {}!", hello);
+    println!("world, {}!", world);
+
+    let s1 = "中国人";
+    let a1 = &s1[0..3];
+    println!("{}",a1);
+
+    let mut s = String::from("hello world");
+
+    let word = first_word(&s);
+
+    // s.clear(); // error!
+
+    println!("the first word is: {}", word);
 }
